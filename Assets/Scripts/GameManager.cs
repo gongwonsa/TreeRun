3b4421ecb7d2ff5ef428a423039a1f2407df9ca9
shapiro7 <57553889+shapiro7@users.noreply.github.com>
2019-11-10 00:15:57 +0900

    타임바 구현/ 게임 메니져 좀 건드림
    타임바 다는 기준은 시간! 켄버스에 타임바 이미지 추가해서 만들었구 currenttime을 hp로 보고 송충이도 그렇게 코딩해야할거같아

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,32 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    float hp;
+    public float playTimeCurrent = 10f;
+    public float playTimeMax = 10;
+    public Image Timebar;
+    
     //List<int> platform = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
         //platform.Add(0);
-        hp = 100;
+       // hp = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckPlayerDie();
+        if (!CheckPlayerDie())
+        {
+            playTimeCurrent -= 1 * Time.deltaTime;
+
+            Timebar.fillAmount = playTimeCurrent / playTimeMax;
+
+
+
+            if(playTimeCurrent<0)
+            {
+                PlayerDie();
+            }
+        }
+
+        
     }
 
     // 플레이어가 죽었는 지 확인하는 함수
-    void CheckPlayerDie()
+    bool CheckPlayerDie()
     {
-        if(hp <= 0)
+        if (playTimeCurrent <= 0)
         {
             PlayerDie();
+            return true;
         }
+        else
+            return false;
     }
 
     void PlayerDie ()
@@ -34,8 +55,8 @@ public class GameManager : MonoBehaviour
         print("게임 오버");
     }
 
-    public float SetHp()
+   /* public float SetHp()
     {
         return hp; 
-    }
+    }*/
 }
