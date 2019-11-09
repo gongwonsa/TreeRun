24ce2dd85f57fd6b2bdbce26e12a3207ab567cc3
gongwonSA <gongwon@ajou.ac.kr>
2019-11-09 18:56:17 +0900

    게임매니저클래스 추가 플랫폼 클래스 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float hp;
+    List<int> platform = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform.Add(0);
+        hp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 플레이어가 죽었는 지 확인하는 함수
+    void CheckPlayerDie()
+    {
+        if(hp <= 0)
+        {
+            PlayerDie();
+        }
+    }
+
+    void PlayerDie ()
+    {
+
+    }
+}
