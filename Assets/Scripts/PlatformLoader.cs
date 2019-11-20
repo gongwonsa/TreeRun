b57892621a0a24d5619c0a1994a85909eac30eb2
shapiro7 <57553889+shapiro7@users.noreply.github.com>
2019-11-20 21:48:40 +0900

    장애물 위치 조정
    위치 조정

--- Assets/Scripts/PlatformLoader.cs
+++ Assets/Scripts/PlatformLoader.cs
@@ -141,9 +141,21 @@ public class PlatformLoader : MonoBehaviour
     {
         if (CreatePossible())
         {
-            int random = Random.Range(0, 3);
-            Instantiate(obstaclePrefabs[random], new Vector3(newPlatform.transform.position.x + 3 * platformWidth, newPlatform.transform.position.y + Random.Range(-1, 2) * (platformHeight ), 0), Quaternion.Euler(0,0,90));
-        }
+            int random = Random.Range(0, 2);
+			// Instantiate(obstaclePrefabs[random], new Vector3(newPlatform.transform.position.x + 3 * platformWidth, newPlatform.transform.position.y + Random.Range(-1, 2) * (platformHeight ), 0), Quaternion.Euler(0,0,90));
+			if (random == 0)
+			{
+				Instantiate(obstaclePrefabs[random], new Vector3(newPlatform.transform.position.x + 3 * platformWidth, newPlatform.transform.position.y + Random.Range(0, 2) * (platformHeight), 0), Quaternion.Euler(0, 0, 90));
+			}
+			else if (random == 1)
+			{
+				Instantiate(obstaclePrefabs[random], new Vector3(newPlatform.transform.position.x + 3 * platformWidth, newPlatform.transform.position.y + Random.Range(2, 3) * (platformHeight), 0), Quaternion.Euler(0, 0, 90));
+			}
+			else
+			{
+				//Instantiate(obstaclePrefabs[random], new Vector3(newPlatform.transform.position.x + 3 * platformWidth, newPlatform.transform.position.y, 0), Quaternion.Euler(0, 0, 90));
+			}
+		}
     }
 
     public float SetWidth() {
